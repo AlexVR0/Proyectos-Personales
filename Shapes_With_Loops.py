53fe411824cae9ce22e791202633c4634cda8609
Alejando Vargas Romo <44343012+AlexVR0@users.noreply.github.com>
2025-02-28 14:07:30 -0600

    Add files via upload

---
+++ Shapes_With_Loops.py
@@ -0,0 +1,16 @@
+Lenght = 10
+ToPrint = "triangle "
+for pos in range(1,Lenght+1):
+    print(ToPrint*pos) #multiplies the letter times de current number in pos 
+
+
+for back in range(Lenght-1,0,-1):
+    print(ToPrint*back)
+
+# Length = 3                   
+# ToPrint = "a"                 
+# for pos in range(1, Length + 1): 
+#     print(ToPrint*pos)         
+   
+# for pos in range(Length, 0, -1):
+#     print(ToPrint*pos)
